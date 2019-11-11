001fb527c9ea4940dda3d2af240f493409def48a
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2019-11-11 09:25:27 +0100

    it broken, please fix it

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ $(document).ready(function() {
 
 function main() {
   mouseEvents();
+  keyEvent();
 }
 
 function mouseEvents() {
-  let
-    articleList = $("article").find(".thumbnail"),
+  let articleList = $("article").find(".thumbnail"),
     contentList = $("article").find(".content");
 
   for (let i = 0; i < articleList.length; i++) {
     $(articleList[i]).click(function() {
       $(contentList[i]).toggle();
+      $(articleList[i]).toggleClass("thumbnail-active");
     });
   }
+}
+
+function keyEvent() {
+  $("*").keypress(function() {
+    console.log("down");
+  });
 }
