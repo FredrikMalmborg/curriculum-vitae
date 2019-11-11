62ccc0b93e4c543e596e16b757f8e686b4172fc3
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2019-11-11 17:44:28 +0100

    it kinda works

--- script.js
+++ script.js
@@ -2,25 +2,42 @@ $(document).ready(function() {
   main();
 });
 
+let artIndex, artActive, articleList, contentList;
+
+
 function main() {
+  globalVariables();
   mouseEvents();
   keyEvent();
 }
 
+function globalVariables() {
+  articleList = $("article").find(".thumbnail");
+  contentList = $("article").find(".content");
+  artActive = false;
+}
+
 function mouseEvents() {
-  let articleList = $("article").find(".thumbnail"),
-    contentList = $("article").find(".content");
 
   for (let i = 0; i < articleList.length; i++) {
     $(articleList[i]).click(function() {
       $(contentList[i]).toggle();
       $(articleList[i]).toggleClass("thumbnail-active");
+      $("hori-hr").hide()
+      artIndex = i;
+      artActive = true;
+      console.log("Article Index is: " + artIndex);
     });
   }
 }
 
 function keyEvent() {
-  $("*").keypress(function() {
-    console.log("down");
+  $(document).keypress(function(event) {
+    if (event.keyCode === 13 && artActive) {
+      console.log("toggle");
+      $(contentList[artIndex]).toggle();
+      $(articleList[artIndex]).toggleClass("thumbnail-active");
+      artActive = false;
+    }
   });
 }
